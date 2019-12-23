ed4600fa5b978c62bf6a981efe84266084fd3796
Colin Perkins <csp@csperkins.org>
2019-12-23 09:39:56 +0000

    Update tests

--- src/lib.rs
+++ src/lib.rs
@@ -658,7 +658,7 @@ mod ietfdata_tests {
         let dt = Datatracker::new();
 
         let st = dt.doc_states()?.collect::<Result<Vec<_>, _>>()?;
-        assert_eq!(st.len(), 153);
+        assert_eq!(st.len(), 154);
         Ok(())
     }
 
